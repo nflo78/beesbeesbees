f55887653cab354cccdfead1c5450a081acf7bd1
Noriel <flores1noriel@gmail.com>
2023-04-03 11:35:30 -0700

    Passed all tests and inheritance patterns on all bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+  this.age = 10
+  this.job = 'find pollen'
+  this.canFly = true
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(new Bee)
+ForagerBee.prototype.forage = function (treasure) {
+  this.treasureChest.push(treasure)
+}
+ForagerBee.prototype.constructor = ForagerBee
